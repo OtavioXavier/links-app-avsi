dc31c6eb8eb112a1df87f4ab6425c0c07a4d0b4b
Otavio <otavioxavier.santos@ucsal.edu.br>
2024-08-29 12:55:58 -0300

    :wrench: chore: application links

---
+++ links.ts
@@ -0,0 +1,50 @@
+export const links ={
+  "RN": {
+    "EcoPosto": {
+      "Satisfação": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUOEdQNTVGSVQzWUgwUFo3QjNQSUVISjNRNC4u",
+      "Pré": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxURFBHSkw1VEMxN0dFNVk4VjZLNzJXRFhVWS4u&r64091e1e24ee4603914c8f6af10dc8c8=%22Pr%C3%A9-Teste%22",
+      "Pós": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxURFBHSkw1VEMxN0dFNVk4VjZLNzJXRFhVWS4u&r64091e1e24ee4603914c8f6af10dc8c8=%22P%C3%B3s-Teste%22"
+    }
+  },
+  "PE": {
+    "UME": {
+      "Pré": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxURDBWMDdQU0E2WkdKOUxBVjVJNjhQTlFDTC4u&r64091e1e24ee4603914c8f6af10dc8c8=%22Pr%C3%A9-Teste%22",
+      "Pós": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxURDBWMDdQU0E2WkdKOUxBVjVJNjhQTlFDTC4u&r64091e1e24ee4603914c8f6af10dc8c8=%22P%C3%B3s-Teste%22",
+      "Satisfação": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUODQxUkkxRE5DNlVKM0g0U0E2MEcyNEdLNi4u"
+    },
+    "EspaçoUsinaSolar": {
+      "Satisfação": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxURDhIQTlWN0s3WFRUNVJFQUVOMVdLRkc3TC4u"
+    }
+  },
+  "BRB": {
+    "UME": {
+      "Pré": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUNTVGR1BFSVFNMjRCSFJMT1IzUEpJUDlGTy4u&r64091e1e24ee4603914c8f6af10dc8c8=%22Pr%C3%A9-Teste%22",
+      "Pós": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUNTVGR1BFSVFNMjRCSFJMT1IzUEpJUDlGTy4u&r64091e1e24ee4603914c8f6af10dc8c8=%22P%C3%B3s-Teste%22",
+      "Satisfação": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUQ0k5VjdTS1hMUUpLR0Q5NUswQ1VFSU81OC4u"
+    }
+  },
+  "SP": {
+    "UME": {
+      "Pré": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUMVFLNkg2Q1FVVktHSFA4UTVKQ0xJNUgxVS4u&r64091e1e24ee4603914c8f6af10dc8c8=%22Pr%C3%A9-Teste%22",
+      "Pós": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUMVFLNkg2Q1FVVktHSFA4UTVKQ0xJNUgxVS4u&r64091e1e24ee4603914c8f6af10dc8c8=%22P%C3%B3s-Teste%22",
+      "Satisfação": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUMEhWUFhLQU1BTkpQNzJRU1FFSjFNNzFZNS4u"
+    }
+  },
+  "BA": {
+    "EspacoMaker": {
+      "Pré": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUQURRWEFaR0FPR0IzWDFMSk00TDhLWkk5Vi4u&rd2dc9c58a12c4be2b2dd151301e6d5e8=%22Pr%C3%A9-teste%22&re0802dcffa2a47a2a119899631e35544=%22Arduino%22",
+      "Pós": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUQURRWEFaR0FPR0IzWDFMSk00TDhLWkk5Vi4u&rd2dc9c58a12c4be2b2dd151301e6d5e8=%22P%C3%B3s-teste%22&re0802dcffa2a47a2a119899631e35544=%22Arduino%22",
+      "Satisfação": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxURUhTVlVLSVlQMzJJRzk3T0I5N0xVQzQ4Ri4u"
+    },
+    "UME": {
+      "Pré": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUN1c4MkZFNE8wVzIzTDRUSlM5UjI0OEM4Sy4u&r64091e1e24ee4603914c8f6af10dc8c8=%22Pr%C3%A9-Teste%22",
+      "Pós": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUN1c4MkZFNE8wVzIzTDRUSlM5UjI0OEM4Sy4u&r64091e1e24ee4603914c8f6af10dc8c8=%22P%C3%B3s-Teste%22",
+      "Satisfação": "https://forms.office.com/r/G1gvqpu4cv"
+    },
+    "Museu": {
+      "Pré": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUMzNaN1dFWlRKU0dBVUlNV1FLM0E1REw1Qi4u&r64091e1e24ee4603914c8f6af10dc8c8=%22Pr%C3%A9-Teste%22",
+      "Pós": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxUMzNaN1dFWlRKU0dBVUlNV1FLM0E1REw1Qi4u&r64091e1e24ee4603914c8f6af10dc8c8=%22P%C3%B3s-Teste%22",
+      "Satisfação": "https://forms.office.com/Pages/ResponsePage.aspx?id=3LdXMkel6USZYsUDx5X2CEs5uG60xS5Mm8XP6V18ksxURUhTVlVLSVlQMzJJRzk3T0I5N0xVQzQ4Ri4u"
+    }
+  }
+}
